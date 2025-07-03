26196159cec5b5a0f0c8976584f4a7ce2d326a98
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2025-07-02 22:48:41 -0400

    more game logic

--- src/game.js
+++ src/game.js
@@ -1,13 +1,26 @@
+
 class Player {
-    
+    constructor(name, token, ai = false) {
+        this.name = name;
+        this.token = token;
+        this.ai = ai;
+    } 
+}
+class AIPlayer extends Player {
+    constructor(name, token, difficulty = "hard") {
+        super(name, token, true);
+        this.difficulty  = difficulty;
+    }
 }
 
 class Game {
-    constructor() {
+    constructor(player1, player2) {
         this.board = this.createBoard();
+        this.players = [player1, player2];
+        this.currentPlayer = 0;
     }
 
-    createBoard() { //  returns 6x7 game board , 0 = empty, R = Red player, Y = Yellow Player
+    createBoard() { //  returns 6x7 game board , 0 = empty, 1 = Red player, 2 = Yellow Player
         const rows = 6;
         const cols = 7;
         const board = Array.from({length: rows }, ()=> Array(cols).fill(0));
@@ -15,8 +28,30 @@ class Game {
         return board;
     }
 
-}
+    dropDisc(col) {
 
-const game = new Game();
+    }
+    
+    checkWin() {
+
+    }
+
+    switchPlayer() {
+        this.currentPlayer = 1 - this.currentPlayer;
+    }
+    
+    play() {
+        console.log(player1, player2);
+        console.log(this.players[this.currentPlayer]);
+        this.switchPlayer();
+        console.log(this.players[this.currentPlayer]);
+        
+    }
+}
+const player1 = new Player("Player 1", 1);
+const player2 = new Player("Player 2", 2);
+const game = new Game(player1, player2);
 
 export default game;
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "./styles.css";
 import game from "./game";
 
+
+
 console.log(game.board)
+game.play();
