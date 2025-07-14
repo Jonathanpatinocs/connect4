bf5d75c9519d4ebe2fa0423f7c95a3d83e98f1a3
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2025-07-13 20:18:49 -0400

    check for win horizontally

--- src/game.js
+++ src/game.js
@@ -33,27 +33,75 @@ class Game {
         console.log(token);
         
         let found = false;
-        for(let i = 0; i < 6; i++) {
+        for(let i = 0; i < 6; i++) { // drops the disc at the next available spot in the column
             if (!found){
-                if (i == 5 ) {
+                if (i == 5 ) { // no discs in a column case
                     this.board[i][col] = token;
+                    this.checkWin(i, col);
                     found = true;
                 }
-                else if (this.board[i][col] == 0) {
-                    if(this.board[i + 1][col] != 0) {
+                else if (this.board[i][col] == 0) {   // if next spot is unavailable, drop disc at the current spot
+                    if(this.board[i + 1][col] != 0) {   
                         this.board[i][col] = token;
+                        this.checkWin(i, col);
                         found = true;
                     }
-                }
-                
+                }  
             }
         }
         console.log(this.board);
         this.switchPlayer();
     }
     
-    checkWin() {
+    checkWin(row, col) { // checks win by looking at the surrounding tokens of the last placed token
+        let token = this.players[this.currentPlayer].token;
+        if (this.checkHorizontal(row, col)) {
+            console.log(`${token} wins`);
+            
+            return true;
+        };
+        
+    }
 
+    checkHorizontal(row, col) {
+        let token = this.players[this.currentPlayer].token;
+        let x = 1; // count of tokens in a row
+        let leftBlocked = false;
+        let leftOfToken = 0;
+        while(!leftBlocked) { //
+            leftOfToken++;
+            if(this.board[row, col - leftOfToken] !== null && this.board[row, col - leftOfToken] == token) {
+                x++;
+            }
+            else {
+                leftBlocked = true;
+            }
+        }
+        let rightBlocked = false;
+        let rightOfToken = 0;
+        while(!rightBlocked) {
+            if(this.board[row, col + rightOfToken] !== null && this.board[row, col + rightOfToken] == token) {
+                x++;
+            }
+            else {
+                rightBlocked = true;
+            }
+        }
+        if( x => 4) {
+            return true;
+        }
+    }
+
+    checkVertical(row, col) {
+        let token = this.players[this.currentPlayer].token;
+    }
+
+    checkLeftDiagonal(row, col) {
+        let token = this.players[this.currentPlayer].token;
+    }
+
+    checkRightDiagonal(row, col) {
+        let token = this.players[this.currentPlayer].token;
     }
 
     switchPlayer() {
@@ -66,6 +114,14 @@ class Game {
         console.log(this.players[this.currentPlayer]);
         this.switchPlayer();
         console.log(this.players[this.currentPlayer]);
+        this.dropDisc(1)
+        this.dropDisc(1)
+        this.dropDisc(2)
+        this.dropDisc(2)
+        this.dropDisc(3)
+        this.dropDisc(3)
+        this.dropDisc(4)
+        
          
     }
 }
