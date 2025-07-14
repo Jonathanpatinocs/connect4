7db09cde037df87dc33b18ffb5743ba0256ce133
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2025-07-13 21:42:09 -0400

    fix check win conditions

--- src/game.js
+++ src/game.js
@@ -30,7 +30,7 @@ class Game {
 
     dropDisc(col) {
         let token = this.players[this.currentPlayer].token;
-        console.log(token);
+        
         
         let found = false;
         for(let i = 0; i < 6; i++) { // drops the disc at the next available spot in the column
@@ -55,23 +55,36 @@ class Game {
     
     checkWin(row, col) { // checks win by looking at the surrounding tokens of the last placed token
         let token = this.players[this.currentPlayer].token;
-        if (this.checkHorizontal(row, col)) {
-            console.log(`${token} wins`);
-            
-            return true;
-        };
+        if (this.checkHorizontal(row, col) == true) {
+            console.log(`${token} win horizontal`)
+        }
+        if (this.checkVertical(row, col) == true) {
+            console.log(`${token} win vertical`)
+        }
+        if (this.checkRightDiagonal(row, col) == true) {
+            console.log(`${token} win right diagonal`)
+        }
+        if (this.checkLeftDiagonal(row, col) == true) {
+            console.log(`${token} win left diagonal`)
+        }
+    
         
     }
 
     checkHorizontal(row, col) {
         let token = this.players[this.currentPlayer].token;
-        let x = 1; // count of tokens in a row
+        let x = 1; // count of tokens horizontally
         let leftBlocked = false;
         let leftOfToken = 0;
-        while(!leftBlocked) { //
+        while(!leftBlocked) { // loops to count left tokens until it hits the edge or hits the opposing players token
             leftOfToken++;
-            if(this.board[row, col - leftOfToken] !== null && this.board[row, col - leftOfToken] == token) {
-                x++;
+            if (!(col - leftOfToken < 0)) {
+                if(this.board[row][col - leftOfToken] == token) {
+                    x++;
+                }
+                else {
+                    leftBlocked = true;
+                }
             }
             else {
                 leftBlocked = true;
@@ -79,29 +92,139 @@ class Game {
         }
         let rightBlocked = false;
         let rightOfToken = 0;
-        while(!rightBlocked) {
-            if(this.board[row, col + rightOfToken] !== null && this.board[row, col + rightOfToken] == token) {
-                x++;
+        while(!rightBlocked) { // loops to count right tokens until it hits the edge or hits the opposing players token
+            rightOfToken++;
+            if(!(col + rightOfToken > 6)) {
+                if(this.board[row][col + rightOfToken] == token) {
+                    x++;
+                }
+                else {
+                    rightBlocked = true;
+                }
             }
             else {
                 rightBlocked = true;
             }
+            
         }
-        if( x => 4) {
+        if( x >= 4) {
+           
+            
+            return true;
+        }
+       
+    }
+
+    checkVertical(row, col) { // 
+        let token = this.players[this.currentPlayer].token;
+        let x = 1; // count of tokens vertically
+        
+        let bottomBlocked = false;
+        let bottomOfToken = 0;
+        while(!bottomBlocked) { // loops to count bottom tokens until it hits the edge or hits the opposing players token
+            bottomOfToken++;
+            if (!(row + bottomOfToken > 5)) {
+                if(this.board[row + bottomOfToken][col] == token) {
+                    x++;
+                }
+                else {
+                    bottomBlocked = true;
+                }
+            }
+            else {
+                bottomBlocked = true;
+            }
+        }
+        if( x >= 4) {
             return true;
         }
     }
 
-    checkVertical(row, col) {
-        let token = this.players[this.currentPlayer].token;
-    }
-
     checkLeftDiagonal(row, col) {
         let token = this.players[this.currentPlayer].token;
+        let x = 1; // count of tokens diagonally (left)
+        let topLeftBlocked = false;
+        let topLeftOfToken = 0;
+        while(!topLeftBlocked) { // loops to count top left tokens until it hits the edge or hits the opposing players token
+            topLeftOfToken++;
+            if (!(row - topLeftOfToken < 0) && !(col - topLeftOfToken < 0)) {
+                if(this.board[row - topLeftOfToken][col - topLeftOfToken] == token) {
+                    x++;
+                }
+                else {
+                    topLeftBlocked = true;
+                }
+            }
+            else {
+                topLeftBlocked = true;
+            }
+        }
+        let bottomRightBlocked = false;
+        let bottomRightOfToken = 0;
+        while(!bottomRightBlocked) { // loops to count bottom right tokens until it hits the edge or hits the opposing players token
+            bottomRightOfToken++;
+            if(!(row + bottomRightOfToken > 5) && !(col + bottomRightOfToken > 6)) {
+                if(this.board[row + bottomRightOfToken][col + bottomRightOfToken] == token) {
+                    x++;
+                }
+                else {
+                    bottomRightBlocked = true;
+                }
+            }
+            else {
+                bottomRightBlocked = true;
+            }
+        }
+        if( x >= 4) {
+            return true;
+        }
+        
+
     }
 
     checkRightDiagonal(row, col) {
         let token = this.players[this.currentPlayer].token;
+        let x = 1; // count of tokens diagonally (right)
+        let topRightBlocked = false;
+        let topRightOfToken = 0;
+        while(!topRightBlocked) { // loops to count top right tokens until it hits the edge or hits the opposing players token
+            topRightOfToken++;
+            if (!(row - topRightOfToken < 0) && !(col + topRightOfToken > 6)) // checks if spot is out of bounds
+            {
+                if(this.board[row - topRightOfToken][col + topRightOfToken] == token) {
+                    x++;
+                }
+                else {
+                    topRightBlocked = true;
+                }
+            }
+            else {
+                topRightBlocked = true;
+            }
+        }
+        let bottomLeftBlocked = false;
+        let bottomLeftOfToken = 0;
+        while(!bottomLeftBlocked) { // loops to count bottom right tokens until it hits the edge or hits the opposing players token
+            
+            bottomLeftOfToken++;
+            if (!(row + bottomLeftOfToken > 5) && !(col - bottomLeftOfToken < 0 )) { // checks if spot is out of bounds
+                if(this.board[row + bottomLeftOfToken][col - bottomLeftOfToken] == token) {
+                    x++;
+                }
+                else {
+                    bottomLeftBlocked = true;
+                }
+            }
+            else {
+                bottomLeftBlocked = true;
+            }
+        }
+  
+        if( x >= 4) {
+            
+            return true;
+        }
+        
     }
 
     switchPlayer() {
@@ -114,9 +237,12 @@ class Game {
         console.log(this.players[this.currentPlayer]);
         this.switchPlayer();
         console.log(this.players[this.currentPlayer]);
+        this.dropDisc(0)
         this.dropDisc(1)
         this.dropDisc(1)
         this.dropDisc(2)
+        this.dropDisc(2)
+        this.dropDisc(3)
         this.dropDisc(2)
         this.dropDisc(3)
         this.dropDisc(3)
